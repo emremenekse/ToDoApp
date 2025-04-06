c3bb684b75fd2fbdfe000e7385402c501af2de40
umut emre menekşe <emreemenekse@gmail.com>
2025-04-06 22:12:09 +0300

    Tags Table Added

---
+++ src/Infrastructure/Persistence/Configurations/TagConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Todo_App.Domain.Entities;
+
+namespace Todo_App.Infrastructure.Persistence.Configurations;
+public class TagConfiguration : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder.Property(t => t.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(t => t.Color)
+            .HasMaxLength(20);
+    }
+}
